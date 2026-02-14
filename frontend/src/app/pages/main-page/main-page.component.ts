3a0ac87fc523b44a57d6ae7a17fd64e4b2127af1
arm1n18 <bachinskijdenis@gmail.com>
2026-02-14 21:22:40 +0200

    main-page

--- frontend/src/app/pages/main-page/main-page.component.ts
+++ frontend/src/app/pages/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MainLayout } from "../../layouts/main-layout/main-layout";
+import { CustomButtonComponent } from "../../components/ui";
+import { Footer } from "../../layouts/footer/footer";
 
 @Component({
   selector: 'app-main-page',
-  imports: [MainLayout],
+  imports: [MainLayout, CustomButtonComponent, Footer],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
